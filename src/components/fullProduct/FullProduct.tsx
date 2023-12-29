7b3a930fc54efc88338641035d63953877eb2408
Amin Tootoonchi <toutounchi.ma@gmail.com>
2023-12-29 18:32:16 +0330

    Shows more detail of Products

--- src/components/fullProduct/FullProduct.tsx
+++ src/components/fullProduct/FullProduct.tsx
@@ -1,5 +1,7 @@
 import { Product } from "../../api/products";
 import styles from "./FullProduct.module.css";
+import likeIcon from "../../assets/like.svg";
+import explorIcon from "../../assets/explor.svg";
 
 interface FullProductsProps {
   product: Product;
@@ -9,7 +11,17 @@ export function FullProduct(props: FullProductsProps) {
   return (
     <div className={styles.container}>
       <img src={props.product.image} />
-      {"FullProduct"}
+      <span>{props.product.title}</span>
+      <span>{props.product.description}</span>
+      <span className={styles.price}>{`$${props.product.price}`}</span>
+      <div className={styles.float}>
+        <div>
+          <img src={explorIcon} />
+        </div>
+        <div>
+          <img src={likeIcon} />
+        </div>
+      </div>
     </div>
   );
 }
